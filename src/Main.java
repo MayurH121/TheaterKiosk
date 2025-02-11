69ac66611798bfdd1d8f857180355c36d723fbd2
Mayur <mayurharisinghani@gmail.com>
2025-02-10 18:20:28 -0700

    Task 4 complete

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner in = new Scanner(System.in);
+        int age = 0;
+        System.out.println("Please enter your age: ");
+        age =in.nextInt();
+        if (age > 20) {
+            System.out.println("You get a wrist band!");
+        }
     }
 }
